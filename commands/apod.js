420a46c8321c4d6641f961070096be60d0211414
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-03-13 15:14:34 -0500

    Let user select a date to search

--- commands/apod.js
+++ commands/apod.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const config = require('../json/config.json');
 
-var date = new Date().toISOString().slice(0, 10)
+var date = new Date().toISOString().slice(0, 10);
 
 let options = {json: true};
 
@@ -16,10 +16,23 @@ module.exports =
     description: 'Astronomy Picture of the Day',
     execute(message, args, client) 
     {
+        let data = message.content.split(" ");
+        data.shift();
+        data = data.join(" ");
+
+        //check if a date is included in the message
+        if (data === "")
+        {
+            message.channel.send("No date in message, using default date");
+        }
+        else if (date)
+        {
+            url = `https://api.nasa.gov/planetary/apod?api_key=${config.nasakey}&date=${data}&concept_tags=True"`;
+        }
 
         request(url, options, (res, json) => 
         {
-            if( json.body.hasOwnProperty('msg')) //This catches the message from an API, could be an error due to date format or theres no image for the set date
+            if(json.body.hasOwnProperty('msg')) //This catches the message from an API, could be an error due to date format or theres no image for the set date
             {
                 var embed = new Discord.MessageEmbed()
                 .setTitle('Astronomy Picture of the Day')
@@ -46,5 +59,12 @@ module.exports =
                 message.channel.send(json.body.url);
             }
         })
+        resetDate();
     }
+}
+
+function resetDate()
+{
+    date = new Date().toISOString().slice(0, 10);
+    url = `https://api.nasa.gov/planetary/apod?api_key=${config.nasakey}&date=${date}&concept_tags=True"`;
 }
