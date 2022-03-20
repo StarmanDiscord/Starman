d290e5c868226848cb89c9ef9d4634ae3e4b3195
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-03-20 16:23:57 -0400

    Add files via upload

--- commands/announcement.js
+++ commands/announcement.js
@@ -8,26 +8,31 @@ module.exports =
     description: 'Send an announcement',
     execute(message, args, client) 
     {
-        if (message.author.id ==config.owner)
+        var server = message.guild.id;
+        if (server = "937433589820309558")
         {
-            let user = message.member.displayName
-            let sentence = message.content.split(" ");
-            sentence.shift();
-            sentence = sentence.join(" ")
+            if (message.author.id ==config.owner)
+            {
+                let user = message.member.displayName
+                let sentence = message.content.split(" ");
+                sentence.shift();
+                sentence = sentence.join(" ")
+        
+                message.channel.send("@everyone");
     
-            message.channel.send("@everyone");
-
-            var embed = new Discord.MessageEmbed()
-            .setTitle("Starman Announcement")
-            .setDescription(sentence)
-            .setFooter("This message was made by " + user)
-            .setColor('BLACK');
-            message.delete();
-            message.channel.send(embed);
+                var embed = new Discord.MessageEmbed()
+                .setTitle("Starman Announcement")
+                .setDescription(sentence)
+                .setFooter("This message was made by " + user)
+                .setColor('BLACK');
+                message.delete();
+                message.channel.send(embed);
+            }
+            else
+            {
+                channel.message.send("You do not have permissions to send announcements.");
+            }
         }
-        else
-        {
-            channel.message.send("You do not have permissions to send announcements.");
-        }
+        else return;
     }
 }
